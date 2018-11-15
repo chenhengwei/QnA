5554fc68871917474ff4be92c31d2fbfd35cc712
chenhengwei <onlylimit@gmail.com>
2018-11-15 20:27:55 +0800

    Initial Commit

---
+++ QnA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QnA
+//
+//  Created by CHEN WESLEY on 2018/11/15.
+//  Copyright © 2018 CHEN WESLEY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
